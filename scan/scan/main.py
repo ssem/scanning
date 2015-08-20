111380f16b8c1cbc32d4323eaa5e7babb51f3710
ssem <ssem@tacnetsol.com>
2015-08-20 17:58:11 -0400

    bug fix and added time estimate
    fixed bug causing valid ranges to be deleted

--- scan/scan/main.py
+++ scan/scan/main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+from datetime import datetime
+from datetime import timedelta
 from scan.scan_server import Scan_Server
 from scan.listen_server import Listen_Server
 
@@ -9,14 +11,18 @@ class Main:
         self.scan_server = Scan_Server()
         self.listen_server = Listen_Server()
 
-    def _print_status(self):
+    def _print_status(self, rate):
         total = self.scan_server.total
         attempts = self.scan_server.attempts
+        seconds = (total - attempts) / rate
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        estimate = "%s:%s:%s" % (h, m, s)
         try:percent = "{:.0f}%".format(float(attempts)/float(total) * 100)
         except:percent = "0%"
         sys.stdout.write("\b" * 100)
-        sys.stdout.write("received: %s sent:%s total:%s percent:%s" % (
-            self.listen_server.received, attempts, total, percent))
+        sys.stdout.write("received: %s sent:%s total:%s percent:%s estimate:%s" % (
+            self.listen_server.received, attempts, total, percent, estimate))
         sys.stdout.flush()
 
     def run(self, range_dir, port_file, rate, outfile):
@@ -24,7 +30,7 @@ class Main:
             self.listen_server.listen(range_dir, outfile)
             self.scan_server.scan(range_dir, port_file, rate)
             while self.scan_server.is_alive():
-                self._print_status()
+                self._print_status(rate)
                 time.sleep(1)
             sys.stdout.write("\n")
             for x in reversed(xrange(1,5)):
@@ -32,7 +38,7 @@ class Main:
                 sys.stdout.write("waiting %s..." % x)
                 sys.stdout.flush()
                 time.sleep(1)
-            self._print_status()
+            self._print_status(rate)
             sys.stdout.write("\n")
             os.chmod(outfile, 0666)
         except KeyboardInterrupt:exit()
