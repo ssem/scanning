36636b75d18de33341520027b1b72434451c43e8
steve <ssem30@gmail.com>
2017-03-02 00:36:17 +0000

    masscan is writing half lines to output file

--- fastscan/fastscan/main.py
+++ fastscan/fastscan/main.py
@@ -15,30 +15,30 @@ class Main:
         self.banners = Helper_Class()
         self.mass_output = ""
 
+    def _is_masscan_running(self):
+        status = self.scan_server.poll()
+        if status is not None:
+            return False
+        return True
+
     def _finger_print(self):
-        while not os.path.exists(self.mass_output):
-            time.sleep(1)
-        f = open(self.mass_output, 'r')
-        while True:
-            try:
-                line = f.readline()
-                status = self.scan_server.poll()
-                if line and not line.startswith("#"):
-                    try:
-                        args = line.split(" ")
+        scanned = {}
+        while not os.path.exists(self.mass_output):time.sleep(1)
+        while self._is_masscan_running():
+            for line in open(self.mass_output, 'r'):
+                try:
+                    args = line.split(" ")
+                    ip_port = "%s:%s" % (args[3], args[2])
+                    if ip_port not in scanned:
+                        scanned[ip_port] = ""
                         result = self.banners.scan_port(args[3], args[2])
                         result['country'] = self.country_lookup.find(args[3])
                         yield result
-                    except Exception as e:
-                        print "[ERROR] %s" % e
-                else:
-                    if status is not None:
-                        f.close()
-                        break
-                    time.sleep(.1)
-            except KeyboardInterrupt:return
-            except Exception as e:
-                print  "[ERROR] %s" % e
+                except IndexError:pass
+                except KeyboardInterrupt:return
+                except Exception as e:
+                    print  "[ERROR] %s" % e
+            time.sleep(180)
 
     def run(self, range_dir, port_file, rate, iface, output):
         self.country_lookup.add_range_dir(range_dir)
