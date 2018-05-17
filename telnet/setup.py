3b0d3f11dd0b175f16347381295c8948c4b7410a
ssem <you@example.com>
2018-05-17 14:31:58 +0000

    replaced telnet with hydra

--- telnet/setup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from distutils.core import setup
-
-setup(name="burtetelnet",
-      version="alpha",
-      author="ssem",
-      scripts=["bin/brutetelnet"],
-      packages=["brutetelnet"],)
